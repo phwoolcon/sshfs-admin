5f33974abeb5a8a26e399b6ffcf4ec5936370a9e
Christopher CHEN <fishdrowned@gmail.com>
2019-09-16 17:55:35 +0800

    refactor: rearrange main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,21 +12,6 @@ import (
 var GinMode = ""
 var Version = "dev"
 
-func setupRouter(router *gin.Engine) {
-	router.Static("/admin", "./web/admin")
-	router.LoadHTMLFiles("./web/admin/404.html", "./web/admin/download.html")
-	router.NoRoute(base.Route404)
-
-	apiRouter := router.Group("/api")
-	apiRouter.Use(base.SessionMiddleware())
-
-	auth.SetupRouter(apiRouter)
-	depts.SetupRouter(apiRouter)
-	users.SetupApiRouter(apiRouter)
-	users.SetupFrontRouter(router)
-	system.SetupRouter(apiRouter)
-}
-
 func main() {
 	gin.SetMode(GinMode)
 	accessStatus := base.SshfsRootAccess()
@@ -48,4 +33,19 @@ func main() {
 		go engine.RunTLS(":8443", tlsCertFile, tlsKeyFile)
 	}
 	engine.Run(":8000")
+}
+
+func setupRouter(router *gin.Engine) {
+	router.Static("/admin", "./web/admin")
+	router.LoadHTMLFiles("./web/admin/404.html", "./web/admin/download.html")
+	router.NoRoute(base.Route404)
+
+	apiRouter := router.Group("/api")
+	apiRouter.Use(base.SessionMiddleware())
+
+	auth.SetupRouter(apiRouter)
+	depts.SetupRouter(apiRouter)
+	users.SetupApiRouter(apiRouter)
+	users.SetupFrontRouter(router)
+	system.SetupRouter(apiRouter)
 }
